573630a0b9a90b2dad4e16e4582bc3e34680f038
Sophie-134 <sofiammendizabal@gmail.com>
2020-07-25 03:07:55 -0300

    Se cambia el pom para mejorar la compatibilidad con la nueva version de java. Ademas se agrego la creacion de usuarios y el jwtoken para tener autenticacion.

--- src/test/java/ar/com/ada/api/pooflixmongo/DemoApplicationTests.java
+++ src/test/java/ar/com/ada/api/pooflixmongo/DemoApplicationTests.java
@@ -1,13 +1,1 @@
-package ar.com.ada.api.pooflixmongo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DemoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
